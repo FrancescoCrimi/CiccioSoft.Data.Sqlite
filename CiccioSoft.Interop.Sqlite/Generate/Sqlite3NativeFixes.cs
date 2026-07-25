c946f476e2d727a3ff1a0aa9e4642e5e2cc73351
Francesco Crimi <francrim@hotmail.com>
2026-07-25 16:04:36 +0200

    cleanup and delete generate.fsx

--- CiccioSoft.Interop.Sqlite/Generate/Sqlite3NativeFixes.cs
+++ CiccioSoft.Interop.Sqlite/Generate/Sqlite3NativeFixes.cs
@@ -8,12 +8,6 @@ namespace CiccioSoft.Interop.Sqlite
 {
     public static unsafe partial class NativeMethods
     {
-        // // Definisci qui la stringa reale in modo pulito e moderno
-        // internal const string SQLITE_DLL = "e_sqlite3";
-        // internal const string SQLITE_DLL = "libsqlite3-0";
-        // internal const string SQLITE_DLL = "winsqlite3";
-        // internal const string SQLITE_DLL = "sqlite3";
-
         [NativeTypeName("#define SQLITE_STATIC ((sqlite3_destructor_type)0)")]
         public static readonly delegate* unmanaged[Cdecl]<void*, void> SQLITE_STATIC = ((delegate* unmanaged[Cdecl]<void*, void>)(0));
 
